4c3166be4da6b85451b2d670770381a23faf940c
Jeroen Van der Straeten <jeroen.vanderstraeten@ugent.be>
2019-04-08 11:30:31 +0200

    started screen

--- LaserChess.py
+++ LaserChess.py
@@ -1,4 +1,25 @@
-import pyglet
+try:
+    import time
+    import pyglet
 
-ok pas dees aan
-en dit ook
+    class Window(pyglet.window.Window):
+        def __init__(self, *args, **kwargs):
+            #inintialiseer schermpje
+            super().__init__(*args, **kwargs)
+
+        def draw(self):
+            self.clear()
+
+        def update(self, dt):
+            pass
+
+
+
+    #start game
+    if __name__ == "__main__":
+        a = Window(height=500, width=500, resizable=False)
+        pyglet.app.run()
+except Exception as e:
+    print(e)
+    time.sleep(5)
+        
